2450e5b46d33caf9adfe36d752af0484e24e51c1
Mariana Becerril <bec_mbecerril@mcp.es>
2024-05-09 12:43:44 +0200

    changes of section 21

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,28 @@
+import Header from "./Header";
+import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview"
+import { Outlet, useNavigation } from "react-router-dom";
+
+function AppLayout() {
+const navigation = useNavigation();
+//console.log(navigation)
+const isLoading = navigation.state === "loading";
+
+    return(
+        <div>
+            {isLoading && <Loader />}
+            <Header />
+
+            <main>
+                <Outlet />
+            </main>
+
+            <CartOverview />
+            
+
+
+        </div>
+    )
+}
+
+export default AppLayout
